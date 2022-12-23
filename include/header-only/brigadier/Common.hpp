1ad872f4cd42c25e7ca37a6f4c28885b59b0c4b4
Tomasz Karpiński <80130528+TomashuTTTT7@users.noreply.github.com>
2022-12-23 13:01:39 +0100

    support tchar

--- include/header-only/brigadier/Common.hpp
+++ include/header-only/brigadier/Common.hpp
@@ -72,17 +72,36 @@ namespace brigadier::detail
         return s3;
     };
 }
-
 #define BRIGADIER_LITERAL(type, s_literal) brigadier::detail::literal_type(type(), s_literal, L##s_literal, u##s_literal, U##s_literal)
 
+#ifdef UNICODE
 #define BRIGADIER_SPECIALIZE_BASIC(type) \
 using type##A = type<char>;              \
+using type##T = type<wchar_t>;           \
 using type##W = type<wchar_t>;
 
-#define BRIGADIER_SPECIALIZE_BASIC_TEMPL(type)               \
-template<typename... Ts> using type##A = type<char, Ts...>;  \
+#define BRIGADIER_SPECIALIZE_BASIC_TEMPL(type)                 \
+template<typename... Ts> using type##A = type<char, Ts...>;    \
+template<typename... Ts> using type##T = type<wchar_t, Ts...>; \
 template<typename... Ts> using type##W = type<wchar_t, Ts...>;
 
 #define BRIGADIER_SPECIALIZE_BASIC_ALIAS(type, tepl_list, templ_spec) \
 template<tepl_list> using type##A = type<char, templ_spec>;           \
+template<tepl_list> using type##T = type<wchar_t, templ_spec>;        \
 template<tepl_list> using type##W = type<wchar_t, templ_spec>;
+#else
+#define BRIGADIER_SPECIALIZE_BASIC(type) \
+using type##A = type<char>;              \
+using type##T = type<char>;              \
+using type##W = type<wchar_t>;
+
+#define BRIGADIER_SPECIALIZE_BASIC_TEMPL(type)               \
+template<typename... Ts> using type##A = type<char, Ts...>;  \
+template<typename... Ts> using type##T = type<char, Ts...>;  \
+template<typename... Ts> using type##W = type<wchar_t, Ts...>;
+
+#define BRIGADIER_SPECIALIZE_BASIC_ALIAS(type, tepl_list, templ_spec) \
+template<tepl_list> using type##A = type<char, templ_spec>;           \
+template<tepl_list> using type##T = type<char, templ_spec>;           \
+template<tepl_list> using type##W = type<wchar_t, templ_spec>;
+#endif
